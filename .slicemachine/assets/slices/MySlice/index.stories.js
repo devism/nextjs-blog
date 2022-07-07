470829d04e469725ada458cd1c2ddd3ca472a459
devism <dub909@gmail.com>
2022-07-07 11:48:24 -0700

    new changes

---
+++ .slicemachine/assets/slices/MySlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MySlice';
+
+export default {
+  title: 'slices/MySlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"my_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Transform ubiquitous initiatives","spans":[]}],"description":[{"type":"paragraph","text":"Cupidatat aliqua culpa reprehenderit dolor enim consectetur aliquip. Duis ut eu enim pariatur aliquip sint dolor consectetur sit. Est ut consectetur eiusmod nostrud sunt laborum pariatur labore consequat sunt esse do sunt deserunt.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
